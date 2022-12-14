0015a038bf79631eb709201594e67e612f231dd7
Artem Lira <artemlira84@gmail.com>
2022-12-14 21:24:54 +0200

    Сделал адаптив секции AboutWork на странице Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ export default function Home() {
       <DiscountGoods />
       <Advantages />
       <AboutWork />
-      {/* <News /> */}
+      <News />
       {/* <SocialNetworks /> */}
       {/* <Subscription /> */}
     </>

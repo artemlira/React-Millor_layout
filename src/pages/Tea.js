2be5bd6fe27c95612d9acde9d7a9dc90b74f156e
Artem Lira <artemlira84@gmail.com>
2022-12-25 04:32:02 +0200

    Сделал секцию TeaDescription на странице Tea

--- src/pages/Tea.js
+++ src/pages/Tea.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import TeaCard from '../components/Page_Tea/TeaCard/TeaCard';
+import TeaDescription from '../components/Page_Tea/TeaDescription/TeaDescription';
+import TeaNav from '../components/Page_Tea/TeaNav/TeaNav';
 import TeaPath from '../components/Page_Tea/TeaPath/TeaPath';
 import TeaSubscription from '../components/Page_Tea/TeaSubscription/TeaSubscription'
-import CoffeeNav from './../components/Page_Coffee/CoffeeNav/CoffeeNav';
 import CoffeeReviews from './../components/Page_Coffee/CoffeeReviews/CoffeeReviews';
 
 export default function Tea() {
@@ -10,7 +11,8 @@ export default function Tea() {
     <article className='tea'>
       <TeaPath />
       <TeaCard />
-      <CoffeeNav />
+      <TeaNav />
+      <TeaDescription />
       <CoffeeReviews />
       <TeaSubscription />
     </article>
